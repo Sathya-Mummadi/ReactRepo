3b3224afb6c1b2b66b584b69d89f4b7e77e898b6
Sathya-Mummadi <satya.mummadi5@gmail.com>
2023-09-20 16:53:08 +0530

    mynew1

---
+++ assignment/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+function App() {
+  return (
+    <div>
+      <div className="holepage">
+      <center>
+      <h1> Assigment</h1>
+      <h3 className="cssapp" >CSS with inline and className concepts.</h3>
+      <p className="para">You should create a simple component with any HTML upon your interest.
+         In this component you need to implement CSS with inline styling. 
+         Also CSS with external file system (using className).
+      </p>
+      </center>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
+
